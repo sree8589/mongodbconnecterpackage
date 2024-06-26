d99678b9d03da8075715c361dd99cbb459574c03
mahesh <maheshsree98@gmail.com>
2024-06-26 15:57:28 +0530

    newthings added

--- setup.py
+++ setup.py
@@ -14,11 +14,29 @@ def get_requirements(file_path:str)->List[str]:
 
     return requirements
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.4"
+REPO_NAME = "mongodbconnecterpackage"
+PKG_NAME= "mongo-connect"
+AUTHOR_USER_NAME = "mahesh"
+AUTHOR_EMAIL = "maheshsree98@gmail.com"
+
 setup(
-    name='DimondPricePrediction',
-    version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
-    install_requires=["scikit-learn","pandas","numpy"],
-    packages=find_packages()
-)
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=get_requirements('./requirements_dev.txt')
+    )
