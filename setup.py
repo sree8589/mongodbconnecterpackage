9845c1a237a9649c71ba1ed22d992750ef2f5bcf
mahesh <maheshsree98@gmail.com>
2024-06-29 10:42:11 +0530

    new files

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open('README.md', 'r', encoding='utf-8') as f:
 __version__ = "0.0.7"
 REPO_NAME = "mongodbconnectorpkg"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "mahesh"
+AUTHOR_EMAIL = "maheshsree98@gmail.com"
 
 setup(
     name=PKG_NAME,
